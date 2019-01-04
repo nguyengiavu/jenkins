b87256b77573b61e00d4d494aa8f166e87f46907
Vu Nguyen <nguyengiavu@gmail.com>
2019-01-04 13:39:59 +0700

    renew project

---
+++ src/main/java/com/nguyengiavu/microservice/hubbr/HubBrApplication.java
@@ -0,0 +1,14 @@
+package com.nguyengiavu.microservice.hubbr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubBrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubBrApplication.class, args);
+	}
+
+}
+
